844f88ebbb4ac5e00ed8879568c07c30a07b7fde
WLPro929 <101619521+WLPro929@users.noreply.github.com>
2022-08-03 11:19:11 +0700

    Update unit1.js

--- unit1.js
+++ unit1.js
@@ -1,19 +1,21 @@
 function calcAge() {
   var dob = document.getElementById("Birthdate").value;
-  var currDate = new Date(dob);
+  var birthDate = new Date(dob);
+  var birthYear = birthDate.getFullYear()
+  var currDate = new Date();
   var currYear = currDate.getFullYear();
-  var age = currYear - 2022
-  return document.getElementByID("Age").innerHTML = dob
+  var age = currYear - birthYear
+  return document.getElementByID("Age").innerHTML = age
 }
 
 function calcCredit() {
-const score = []
-score[0] = document.getElementById("score1").value;
-score[1] = document.getElementById("score2").value;
-score[2] = document.getElementById("score3").value;
-score[3] = document.getElementById("score4").value;
-score[4] = document.getElementById("score5").value;
-score[5] = document.getElementById("score6").value;
+  const score = []
+  score[0] = document.getElementById("score1").value;
+  score[1] = document.getElementById("score2").value;
+  score[2] = document.getElementById("score3").value;
+  score[3] = document.getElementById("score4").value;
+  score[4] = document.getElementById("score5").value;
+  score[5] = document.getElementById("score6").value;
 }  
 
 function load() {
