3e91a2ddf5990cbe17ea4afa33f5b0fc7c1f947d
WLPro929 <101619521+WLPro929@users.noreply.github.com>
2022-08-03 11:04:13 +0700

    Update unit1.js

--- unit1.js
+++ unit1.js
@@ -1,3 +1,24 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Paragraph changed.";
+function calcAge() {
+var dob = document.getElementById("Birthdate").value;
+var currDate = new Date(dob);
+var currYear = currDate.getFullYear();
+var age = currYear - 
+return document.getElementByID("Age").innerHTML = age
+}
+
+function calcCredit() {
+const score = []
+score[0] = document.getElementById("score1").value;
+score[1] = document.getElementById("score2").value;
+score[2] = document.getElementById("score3").value;
+score[3] = document.getElementById("score4").value;
+score[4] = document.getElementById("score5").value;
+score[5] = document.getElementById("score6").value;
+  
+
+function load() {
+  calcAge();
+  calcCredit();
+  calcComment();
+  calcGPA();
 }
